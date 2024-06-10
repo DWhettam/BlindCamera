c438df7c7a3c6fd8b5fb0ac2072974bb45065f87
DWhettam <dwhettam@gmail.com>
2024-06-10 12:43:01 +0100

    committing all changes

--- EPIC_job.sh
+++ EPIC_job.sh
@@ -5,10 +5,10 @@
 #SBATCH --job-name=DCASE-resnet50
 #SBATCH --mem=100G
 #SBATCH --account=cosc014882
-#SBATCH -t 04-24:00:00
+#SBATCH -t 06-24:00:00
 module load CUDA
 
 
 source /mnt/storage/home/qc19291/miniconda3/bin/activate DCASE
 
-python -u EPIC_resnet.py --epochs 100 --ngpus 2 --window_size 10 --hop_length 5  --batch_size 128 --pretrained True --learning_rate 0.0001 --time_warp 2 --freq_mask 20 --time_mask 20 --augment True --label_type verb #--scheduler Platea
+python -u EPIC_resnet.py --epochs 100 --ngpus 2 --window_size 10 --hop_length 5  --batch_size 128 --pretrained True --learning_rate 0.001 --label_type verb
